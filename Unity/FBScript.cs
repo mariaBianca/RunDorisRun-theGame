e2260f09a10e5450ce178a58092f622436157298
Mika-Matti Savikko <savikko@gmail.com>
2016-11-17 16:09:50 +0100

    added profile pic method
    void DisplayUsername

--- Unity/FBScript.cs
+++ Unity/FBScript.cs
@@ -11,91 +11,120 @@ using System.Collections.Generic;
 using Facebook.Unity;
 using UnityEngine.UI;
 
-public class FBScript : MonoBehaviour {
+public class FBscript : MonoBehaviour
+{
 
 
-	public GameObject DialogLoggedIn;
-	public GameObject DialogLoggedOut;
-	public GameObject DialogUsername;
-	public GameObject DialogProfilePic;
+    public GameObject DialogLoggedIn;
+    public GameObject DialogLoggedOut;
+    public GameObject DialogUsername;
+    public GameObject DialogProfilePic;
 
-	//Initialize the function
-	void Awake () 
-	{
-		FB.Init (setInit, onHideUnity);
-	}
+    //Initialize the function
+    void Awake()
+    {
+        FB.Init(setInit, onHideUnity);
+    }
 
-	//set the Facebook initialization and log accordingly
-	void setInit()
-	{
-		if (FB.IsLoggedIn) {
-			Debug.Log ("Facebook is logged in.");
-		} else {
-			Debug.Log ("Facebook is not logged in.");
-		}
-	DealWithFBMenus (FB.IsLoggedIn);
+    //set the Facebook initialization and log accordingly
+    void setInit()
+    {
+        if (FB.IsLoggedIn)
+        {
+            Debug.Log("Facebook is logged in.");
+        }
+        else
+        {
+            Debug.Log("Facebook is not logged in.");
+        }
+        DealWithFBMenus(FB.IsLoggedIn);
 
-	}
+    }
 
-	//if the game is shown continue, otherwise pause the game
-	void onHideUnity(bool isGameShown)
-	{
-		if (!isGameShown) {
-			Time.timeScale = 0;
-		} else {
-			Time.timeScale = 1;		
-		}
-	}
+    //if the game is shown continue, otherwise pause the game
+    void onHideUnity(bool isGameShown)
+    {
+        if (!isGameShown)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
 
-	//Login function
-	public void FBlogin()
-	{
-		List<string> permissions = new List<string> ();
-		//Facebook in their documentations recommends to not ask too many permissions when you want to login
-		//when you want to extra things down the line, thus only few permissions have been added
-		permissions.Add("public_profile");
+    //Login function
+    public void FBlogin()
+    {
+        List<string> permissions = new List<string>();
+        //Facebook in their documentations recommends to not ask too many permissions when you want to login
+        //when you want to extra things down the line, thus only few permissions have been added
+        permissions.Add("public_profile");
 
-		FB.LogInWithReadPermissions (permissions, AuthCallBack);
-	}
+        FB.LogInWithReadPermissions(permissions, AuthCallBack);
+    }
 
-	//deal with errors
-	void AuthCallBack(IResult result)
-	{
-		if (result.Error != null){
-			Debug.Log(result.Error);
-		} else {
-			if (FB.IsLoggedIn){
-				Debug.Log("Facebook is logged in.");
-			} else {
-				Debug.Log("Facebook is not logged in.");
-			}
-			DealWithFBMenus (FB.IsLoggedIn);
+    //deal with errors
+    void AuthCallBack(IResult result)
+    {
+        if (result.Error != null)
+        {
+            Debug.Log(result.Error);
+        }
+        else
+        {
+            if (FB.IsLoggedIn)
+            {
+                Debug.Log("Facebook is logged in.");
+            }
+            else
+            {
+                Debug.Log("Facebook is not logged in.");
+            }
+            DealWithFBMenus(FB.IsLoggedIn);
 
-		}
-	}
+        }
+    }
 
-	//change Facebook menus accordingly
-	void DealWithFBMenus(bool isLoggedIn)
-	{
-		if (isLoggedIn) {
-			DialogLoggedIn.SetActive (true);
-			DialogLoggedOut.SetActive (false);
-			FB.API ("/me?fields=first_name", HttpMethod.GET, DisplayUsername);
-		} else {
-			DialogLoggedIn.SetActive (false);
-			DialogLoggedOut.SetActive (true);
-		}
-	}
+    //change Facebook menus accordingly
+    void DealWithFBMenus(bool isLoggedIn)
+    {
+        if (isLoggedIn)
+        {
+            DialogLoggedIn.SetActive(true);
+            DialogLoggedOut.SetActive(false);
+            FB.API("/me?fields=first_name", HttpMethod.GET, DisplayUsername);
+            FB.API("/me/picture?type=square&height=128&width=128", HttpMethod.GET, DisplayProfilePic);
+        }
+        else
+        {
+            DialogLoggedIn.SetActive(false);
+            DialogLoggedOut.SetActive(true);
+        }
+    }
 
-	//method that displays the grabbed username
-	void DisplayUsername(IResult result)
-	{
-		Text UserName = DialogUsername.GetComponent<Text> ();
-		if (result.Error == null) {
-			UserName.text = "Hi there, " + result.ResultDictionary["first_name"];
-		} else {
-			Debug.Log (result.Error);
-		}
+    //method that displays the grabbed username
+    void DisplayUsername(IResult result)
+    {
+        Text UserName = DialogUsername.GetComponent<Text>();
+        if (result.Error == null)
+        {
+            UserName.text = "Hi there, " + result.ResultDictionary["first_name"];
+        }
+        else
+        {
+            Debug.Log(result.Error);
+        }
 
-	}
+    }
+    //method that displays the profile picture
+    void DisplayProfilePic(IGraphResult result)
+    {
+        if (result.Texture != null)
+        {
+            Image ProfilePic = DialogProfilePic.GetComponent<Image>();
+            ProfilePic.sprite = Sprite.Create(result.Texture, new Rect(0, 0, 128, 128), new Vector2());
+        }
+    }
 }
