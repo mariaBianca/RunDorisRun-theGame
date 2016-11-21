064cf3b2122ce351d09f08ae27f4e40be4870790
Filip_Isakovski <filip.saver15@gmail.com>
2016-11-21 21:37:55 +0100

    Scripts used to connect Unity3D with the MQTT protocol

---
+++ Unity/ConnectionScripts/MQTT/MqttMovementTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using System.Net;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace UnityStandardAssets._2D
+{
+    [RequireComponent(typeof(PlatformerCharacter2D))]
+    public class MqttMovementTest : MonoBehaviour
+    {
+        private PlatformerCharacter2D m_Character;
+        private bool m_Jump;
+        private mqttTest mqttTest;
+        private String mqttCommand;
+        private MqttMsgPublishEventArgs mqttReceived;
+        private MqttClient mqttClient = new MqttClient(IPAddress.Parse("129.16.155.34"), 1883, false, null);
+        private string clientId = Guid.NewGuid().ToString();
+        static String mqttMessageRec = "";
+
+        private void Awake()
+        {
+            mqttClient.Connect(clientId, "owntracks", "theHub");
+            mqttClient.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+            mqttClient.Subscribe(new string[] { "test" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+            m_Character = GetComponent<PlatformerCharacter2D>();
+
+        }
+
+        String getMqttCommand()
+        {
+            return mqttMessageRec;
+        }
+
+        void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            //mqttMessageRec = System.Text.Encoding.UTF8.GetString(e.Message);
+            Console.WriteLine(mqttMessageRec);
+            //Debug.Log("Received: " + System.Text.Encoding.UTF8.GetString(e.Message));
+        }
+
+        void jumpOnMqtt(object sender, MqttMsgPublishEventArgs e)
+        {
+            if (System.Text.Encoding.UTF8.GetString(e.Message) == "Jump")
+            {
+                m_Jump = true;
+            }
+            //m_Jump = false;
+        }
+
+
+        private void Update()
+        {
+            //mqttClient.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+            Console.WriteLine(mqttMessageRec);
+            if (!m_Jump)
+            {
+                mqttClient.MqttMsgPublishReceived += jumpOnMqtt;
+                // Read the jump input in Update so button presses aren't missed.
+                //m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+                if (mqttMessageRec == "Jump")
+                {
+                    //m_Jump = true;
+                    //Debug.Log("Received: " + mqttMessageRec);
+                }
+                
+            }
+        }
+
+
+        private void FixedUpdate()
+        {
+            // Read the inputs.
+            bool crouch = Input.GetKey(KeyCode.LeftControl);
+            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            // Pass all parameters to the character control script.
+            m_Character.Move(h, crouch, m_Jump);
+            m_Jump = false;
+        }
+    }
+}
---
+++ Unity/ConnectionScripts/MQTT/mqttTest.cs
@@ -0,0 +1,60 @@
+﻿/**
+*Script used to connect test the Unity3D to MongoDB connection.
+*This script utilises the M2Mqtt client library for the MQTT protocol
+*
+*This is a modified script from https://github.com/vovacooper/Unity3d_MQTT/
+*
+*DIT029 H16 Project: Software Architecture for Distributed Systems
+*University of Gothenburg, Sweden 2016
+*/
+using UnityEngine;
+using System.Collections;
+using System.Net;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using uPLibrary.Networking.M2Mqtt.Utility;
+using uPLibrary.Networking.M2Mqtt.Exceptions;
+
+using System;
+
+public class mqttTest : MonoBehaviour {
+	private MqttClient client;
+	// This function initializes the script
+	void Start () {
+        // create an instance of the client 
+        //client = new MqttClient(IPAddress.Parse("129.16.155.34"), 1883, false, null);
+        client = new MqttClient(IPAddress.Parse("127.0.0.1"), 1883, false, null);
+
+        // register to received messages
+        client.MqttMsgPublishReceived += client_MqttMsgPublishReceived; 
+		
+		string clientId = Guid.NewGuid().ToString();
+        //client.Connect(clientId, "owntracks", "theHub");
+        client.Connect(clientId);
+
+        // subscribe to the topic "test/test" with QoS 2 (which sends messages exactly once)
+        client.Subscribe(new string[] { "test/test" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+
+
+    }
+    void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e) 
+	{ 
+
+		Debug.Log("Received: " + System.Text.Encoding.UTF8.GetString(e.Message)  );
+	} 
+	
+	//creates an onGUI button that publishes a message through the MQTT protocol
+	void OnGUI(){
+		if ( GUI.Button (new Rect (20,40,80,20), "Level 1")) {
+			Debug.Log("sending...");
+            client.Publish("test/test", System.Text.Encoding.UTF8.GetBytes("{\"Score_ID\":\"NewPlayer25\", \"Score\":1556, \"Player_ID\":88798765, \"Level_ID\":11}"), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+            Debug.Log("sent");
+		}
+	}
+	// the function Update is called once per frame in Unity
+	void Update () {
+
+
+
+	}
+}
