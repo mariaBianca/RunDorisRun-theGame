988a7e024daa2efbd23ec2030c09a529e6533c91
MayraGeraldine <mayrageraldine@icloud.com>
2016-11-26 23:22:43 +0100

    Chat Interface
    Adding chat interfacd in Unity

---
+++ Unity/ChatGUI/Assets/MovableWindow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class MovableWindow : MonoBehaviour, IBeginDragHandler, IDragHandler {
+
+	public Vector3 offset;
+
+	#region IBeginDragHandler implementation
+	public void OnBeginDrag (PointerEventData eventData)
+	{
+		offset = transform.position - (Vector3)eventData.position;
+
+	}
+	#endregion
+
+	#region IDragHandler implementation
+
+	public void OnDrag (PointerEventData eventData)
+	{
+		transform.position = (Vector3) eventData.position + offset;
+		//will show sthe position of the mouse
+	}
+
+	#endregion
+}
