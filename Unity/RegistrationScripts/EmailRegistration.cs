48059c81c54649a6b1c4983f8fc5fe37de369cbe
Mika-Matti Savikko <savikko@gmail.com>
2016-11-30 23:56:00 +0100

    @mariaBianca Changes to publish and subscribe MQTT
    Changed the publish and subscribe in MQTT

--- Unity/RegistrationScripts/EmailRegistration.cs
+++ Unity/RegistrationScripts/EmailRegistration.cs
@@ -22,237 +22,268 @@ using System.Text;
 
 public class EmailRegistration : MonoBehaviour
 {
-	//MQTT commands
-	//private String mqttCommand;
-	//private MqttMsgPublishEventArgs mqttReceived;
+    private MqttClient mqttClient;
 
-	private MqttClient mqttClient;
+    public GameObject email;
+    public GameObject password;
+    public GameObject confirmPassword;
+    public GameObject username;
+    public GameObject validationText;
 
-	public GameObject email;
-	public GameObject password;
-	public GameObject confirmPassword;
-	public GameObject username;
-	public GameObject validationText;
+    private string infoJson = "";
+    private string Email = "";
+    private string Username = "";
+    private string Password = "";
+    private string ConfirmPassword = "";
+    private string ValidationText = "";
+    private string userID = "";
+    private string MqttMailCheck = "I am empty";
 
-	private string infoJson = "";
-	private string Email = "";
-	private string Username = "";
-	private string Password = "";
-	private string ConfirmPassword = "";
-	private string form = "";
-	private string ValidationText = "";
-	private int userID = 0;
-	private string MqttMailCheck = "";
+    // Use this for initialization of the MQTT client
+    void Start()
+    {
+        //mqttClient = new MqttClient(IPAddress.Parse("129.16.155.34"), 1883, false, null);
+        mqttClient = new MqttClient(IPAddress.Parse("127.0.0.1"), 1883, false, null);
 
-	// Use this for initialization of the MQTT client
-	void Start()
-	{
-		//mqttClient = new MqttClient(IPAddress.Parse("129.16.155.34"), 1883, false, null);
-		mqttClient = new MqttClient(IPAddress.Parse("127.0.0.1"), 1883, false, null);
-		string clientId = Guid.NewGuid().ToString();
-		mqttClient.Connect(clientId);
-		//getMesssage subscribed
-		//mqttClient.MqttMsgPublishReceived += client_MqttMsgPublishReceived; 
-	}
+        string clientId = Guid.NewGuid().ToString();
+        mqttClient.Connect(clientId);
 
-	// Adds the register button and its functionalities
-	public void RegisterButton()
-	{
-		//print ("Registration Successful");
-		bool UN = false; //boolean for the username
-		bool EMV = false; //boolean for the email validation
-		bool EM = false; //boolean for the email existance
-		bool PW  = false; //boolean for th	e password
-		bool CPW = false; //boolean for the confirm of password
-		string hashedEmail = "";
+        //register to message received
+        mqttClient.MqttMsgPublishReceived += MqttClient_MqttMsgPublishReceived;
 
-		//check if username is valid
-		if (Username != "") {
-			UN = true;
-		} else {
-			ValidationText = "Username field is empty!";
-			Debug.LogWarning (ValidationText);
-			validationText.GetComponent<Text> ().text = ValidationText;
-		}
+        //subscribe to the topic "test" with Qos2
+        mqttClient.Subscribe(new string[] { "test" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+    }
 
-		//check if email already exists
-		if (Email != "") {
-			hashedEmail = Hash (Email);
+    // Adds the register button and its functionalities
+    public void RegisterButton()
+    {
+        bool UN = false; //boolean for the username
+        bool EMV = false; //boolean for the email valid  ation
+        bool EM = false; //boolean for the email existance
+        bool PW = false; //boolean for th	e password
+        bool CPW = false; //boolean for the confirm of password
+        string hashedEmail = "";
 
-			if (!checkEmailExistance(hashedEmail)) {
-				EM = true;
-			} else {
-				ValidationText = "Email is already in use.";
-				Debug.LogWarning (ValidationText);
-				validationText.GetComponent<Text> ().text = ValidationText;
-			}
-		} else {
-			ValidationText = "Email field is empty.";
-			Debug.LogWarning (ValidationText);
-			validationText.GetComponent<Text> ().text = ValidationText;
-		}
+        //check if username is valid
+        if (Username != "")
+        {
+            UN = true;
+        }
+        else
+        {
+            ValidationText = "Username field is empty!";
+            Debug.LogWarning(ValidationText);
+            validationText.GetComponent<Text>().text = ValidationText;
+        }
 
-		//check if email is valid
-		if (Email != "") {
-			if (Email.Contains ("@")) {
-				if (Email.Contains (".")) {
-					EMV = true;
-				} else {
-					ValidationText = "Email field is incorrect.";
-					Debug.LogWarning (ValidationText);	
-					validationText.GetComponent<Text> ().text = ValidationText;
+        //check if email is valid or already in use
+        if (Email != "")
+        {
+            hashedEmail = Hash(Email);
+            if (Email.Contains("@"))
+            {
+                if (Email.Contains("."))
+                {
+                    EMV = true;
+                    if (checkEmailExistance(hashedEmail) == false)
+                    {
+                        //if (true){
+                        EM = true;
+                    }
+                    else
+                    {
+                        ValidationText = "Email is already in use.";
+                        Debug.LogWarning(ValidationText);
+                        validationText.GetComponent<Text>().text = ValidationText;
+                    }
+                }
+                else
+                {
+                    ValidationText = "Email field is incorrect.";
+                    Debug.LogWarning(ValidationText);
+                    validationText.GetComponent<Text>().text = ValidationText;
+                }
+            }
+            else
+            {
+                ValidationText = "Email field is incorrect.";
+                Debug.LogWarning(ValidationText);
+                validationText.GetComponent<Text>().text = ValidationText;
+            }
+        }
+        else
+        {
+            ValidationText = "Email field is empty.";
+            Debug.LogWarning(ValidationText);
+            validationText.GetComponent<Text>().text = ValidationText;
+        }
 
-				}
-			} else {
-				ValidationText = "Email field is incorrect.";
-				Debug.LogWarning (ValidationText);
-				validationText.GetComponent<Text> ().text = ValidationText;
-			}
-		} else {
-			ValidationText = "Email field is empty!";
-			Debug.LogWarning (ValidationText);
-			validationText.GetComponent<Text> ().text = ValidationText;
-		}
+        //check if the password is valid
+        if (Password != "")
+        {
+            if (Password.Length > 5)
+            {
+                PW = true;
+            }
+            else
+            {
+                ValidationText = "The password must be at least 6 characters.";
+                Debug.LogWarning(ValidationText);
+                validationText.GetComponent<Text>().text = ValidationText;
+            }
+        }
+        else
+        {
+            ValidationText = "Password field is empty!";
+            Debug.LogWarning(ValidationText);
+            validationText.GetComponent<Text>().text = ValidationText;
+        }
 
-		//check if the password is valid
-		if (Password != "") {
-			if (Password.Length > 5) {
-				PW = true;
-			} else {
-				ValidationText = "The password must be at least 6 characters.";
-				Debug.LogWarning (ValidationText);
-				validationText.GetComponent<Text> ().text = ValidationText;
-			}
-		} else {
-			ValidationText = "Password field is empty!";
-			Debug.LogWarning (ValidationText);
-			validationText.GetComponent<Text> ().text = ValidationText;
-		}
+        //check if password confirmation is valid (same with the password
+        if (ConfirmPassword != "")
+        {
+            if (ConfirmPassword == Password)
+            {
+                CPW = true;
+            }
+            else
+            {
+                ValidationText = "Password and password confirmation field do not match!";
+                Debug.LogWarning(ValidationText);
+                validationText.GetComponent<Text>().text = ValidationText;
+            }
+        }
+        else
+        {
+            ValidationText = "Password confirmation field is empty!";
+            Debug.LogWarning(ValidationText);
+            validationText.GetComponent<Text>().text = ValidationText;
+        }
 
-		//check if password confirmation is valid (same with the password
-		if (ConfirmPassword != "") {
-			if (ConfirmPassword == Password) {
-				CPW = true;
-			} else {
-				ValidationText = "Password and password confirmation field do not match!";
-				Debug.LogWarning (ValidationText);
-				validationText.GetComponent<Text> ().text = ValidationText;
-			}
-		} else {
-			ValidationText = "Password confirmation field is empty!"; 
-			Debug.LogWarning (ValidationText);
-			validationText.GetComponent<Text> ().text = ValidationText;
-		}
+        //hash the password and send the objects to the database
+        if (UN == true && EM == true && PW == true && CPW == true && EMV == true)
+        {
+            string hashedPass = Hash(Password);
+            //store form for local tests
+            //store the data in a form in the following format: UserName, Email:, value of hashedEmail, value of hashedPass
+            //form = (Username+ ", " + Email + ", " + hashedEmail +", " + hashedPass);
+            //System.IO.File.WriteAllText(@"C:/Users/lenovo/Desktop/UnityTest/" + Email + ".txt", form);
 
-		//hash the password and send the objects to the database
-		if (UN == true && EM == true && PW == true && CPW == true && EMV == true){
-			string hashedPass = Hash (Password);
-			//store form for local tests
-			//store the data in a form in the following format: UserName, Email:, value of hashedEmail, value of hashedPass
-			//form = (Username+ ", " + Email + ", " + hashedEmail +", " + hashedPass);
-			//System.IO.File.WriteAllText(@"C:/Users/lenovo/Desktop/UnityTest/" + Email + ".txt", form);
+            //send the user JSON object through MQTT
+            int uID = Random.Range(0, 100000);
+            userID = "" + uID;
+            infoJson = "{\"name\": \"" + Username + "\" " +
+                ",\"user_ID\": " + userID +
+                ",\"hashedEmail\": \"" + hashedEmail + "\" " +
+                ",\"hashedPass\": \"" + hashedPass + "\" " +
+                ",\"typeOfClient\": \"RunDorisRun\" }";
 
-			//send the user JSON object through MQTT
-			userID = Random.Range (0, 100000);
-			infoJson = "{\"name\": \"" + Username + "\" " +
-				",\"user_ID\": " + userID +
-				",\"hashedEmail\": \"" + hashedEmail + "\" " +
-				",\"hashedPass\": \"" + hashedPass + "\" " +
-				",\"typeOfClient\": \"RunDorisRun\" }";      
+            //add it to the database through MQTT
+            if (mqttClient.IsConnected)
+            {
+                //Debug.Log (infoJson);
+                mqttClient.Publish("testStore", System.Text.Encoding.UTF8.GetBytes(infoJson), MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE, true);
+            }
+            else
+            {
+                Debug.Log("MQTT connection does not work.");
+            }
 
-			//add it to the database through MQTT
-			if (mqttClient.IsConnected) {
-				mqttClient.Publish ("testStore", System.Text.Encoding.UTF8.GetBytes (infoJson), MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE, true);
-			} else {
-				Debug.Log ("MQTT connection does not work.");
-			}
+            //clear all the registration fields 
+            email.GetComponent<InputField>().text = "";
+            password.GetComponent<InputField>().text = "";
+            confirmPassword.GetComponent<InputField>().text = "";
+            username.GetComponent<InputField>().text = "";
+            validationText.GetComponent<Text>().text = "";
+            ValidationText = "";
+            UN = false;
+            EM = false;
+            PW = false;
+            CPW = false;
+            ValidationText = "Registration Completed!";
+            Debug.Log(ValidationText);
+            validationText.GetComponent<Text>().text = ValidationText;
 
-			//clear all the registration fields 
-			email.GetComponent<InputField>().text = "";
-			password.GetComponent<InputField>().text = "";
-			confirmPassword.GetComponent<InputField>().text = "";
-			username.GetComponent<InputField> ().text = "";
-			validationText.GetComponent<Text> ().text = "";
-			ValidationText = "";
-			UN = false;
-			EM = false;
-			PW = false;
-			CPW = false;
-			ValidationText = "Registration Completed!";
-			Debug.Log (ValidationText);
-			validationText.GetComponent<Text> ().text = ValidationText;
+        }
+    }
 
-		} 
-	}
+    //returns a boolean that says if the email exists in the db or not
+    bool checkEmailExistance(string hashedEmail)
+    {
 
-	//returns a boolean that says if the email exists in the db or not
-	bool checkEmailExistance(string hashedEmail){
-		//bool check = false;
-		string messageJson = "{\"hashedEmail\": \"" + hashedEmail + "\" }";
-		if (mqttClient.IsConnected) {
-			mqttClient.Publish ("test", System.Text.Encoding.UTF8.GetBytes (messageJson), MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE, true);
-		} else {
-			Debug.Log ("MQTT connection does not work.");
-		}
-		mqttClient.MqttMsgPublishReceived += MqttMsgGetString;
-		Debug.Log ("[ " + hashedEmail + " ]" + "  :  " + MqttMailCheck);
-		if ("[ " + hashedEmail + " ]" == MqttMailCheck) {
-			Debug.Log ("Message: " + MqttMailCheck);
-			return true;
-		} else {
-			Debug.Log ("ERROR!!!");
-		}
-		//mqttClient.Subscribe ("test", System.Text.Encoding.UTF8.GetBytes (email), MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE, true);
-		return false;
-	}
+        //bool check = false;
+        //string messageJson = "{\"hashedEmail\": \"" + hashedEmail + "\" }";
 
-	//hash the password, email or any other data inputted using sha512
-	string Hash(string data){
+        if (mqttClient.IsConnected)
+        {
+            Debug.Log("I am in the fetch part");
+            Debug.Log(MqttMailCheck);
+            return false;
+        }
+        else
+        {
+            Debug.Log("MQTT connection does not work.");
+            return true;
+        }
+    }
 
-		var bytes = new UTF8Encoding ().GetBytes (data);
-		byte[] hashBytes;
-		using (var algorithm = new System.Security.Cryptography.SHA512Managed ()) {
-			hashBytes = algorithm.ComputeHash (bytes);
-		}
-		return Convert.ToBase64String(hashBytes);
-	}
 
-	// Update is called once per frame
-	void Update()
-	{
-		// Move to the next button with TAB.
-		if (Input.GetKeyDown(KeyCode.Tab))
-		{
-			if (email.GetComponent<InputField>().isFocused)
-			{
-				username.GetComponent<InputField>().Select();
-			}
-			if (username.GetComponent<InputField> ().isFocused) 
-			{
-				password.GetComponent<InputField> ().Select ();
-			}
-			if (password.GetComponent<InputField>().isFocused)
-			{
-				confirmPassword.GetComponent<InputField>().Select();
-			}
-		}
+    //hash the password, email or any other data inputted using sha512
+    private string Hash(string data)
+    {
 
-		//get the field of the components
-		Email = email.GetComponent<InputField>().text;
-		Password = password.GetComponent<InputField>().text;
-		ConfirmPassword = confirmPassword.GetComponent<InputField>().text;
-		Username = username.GetComponent<InputField> ().text;
+        var bytes = new UTF8Encoding().GetBytes(data);
+        byte[] hashBytes;
+        using (var algorithm = new System.Security.Cryptography.SHA512Managed())
+        {
+            hashBytes = algorithm.ComputeHash(bytes);
+        }
+        return Convert.ToBase64String(hashBytes);
+    }
 
-		if (Input.GetKeyDown (KeyCode.Return)) {
-			if (Email != "" && Username != "" && Password != "" && ConfirmPassword != "") {
-				RegisterButton ();
-			} 
-		}
+    //handle received messages
+    void MqttClient_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+    {
+        Debug.Log(System.Text.Encoding.UTF8.GetString(e.Message));
+        MqttMailCheck = "I am changed.";
+        //MqttMailCheck = System.Text.Encoding.UTF8.GetString(e.Message);
+    }
 
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        // Move to the next button with TAB.
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (email.GetComponent<InputField>().isFocused)
+            {
+                username.GetComponent<InputField>().Select();
+            }
+            if (username.GetComponent<InputField>().isFocused)
+            {
+                password.GetComponent<InputField>().Select();
+            }
+            if (password.GetComponent<InputField>().isFocused)
+            {
+                confirmPassword.GetComponent<InputField>().Select();
+            }
+        }
 
-	void MqttMsgGetString(object sender, MqttMsgPublishEventArgs e) 
-	{ 
-		MqttMailCheck = System.Text.Encoding.UTF8.GetString(e.Message);
-	}
+        //get the field of the components
+        Email = email.GetComponent<InputField>().text;
+        Password = password.GetComponent<InputField>().text;
+        ConfirmPassword = confirmPassword.GetComponent<InputField>().text;
+        Username = username.GetComponent<InputField>().text;
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (Email != "" && Username != "" && Password != "" && ConfirmPassword != "")
+            {
+                RegisterButton();
+            }
+        }
+
+    }
+
 }
