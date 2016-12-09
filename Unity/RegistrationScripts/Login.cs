6baf270754a2aad3c49e3498fefcf3e76db727de
Battseeyon <bianca.cindroi@gmail.com>
2016-12-09 10:12:03 +0100

    latest changes + @msavikko

--- Unity/RegistrationScripts/Login.cs
+++ Unity/RegistrationScripts/Login.cs
@@ -3,9 +3,16 @@ using System.Collections;
 
 public class Login : MonoBehaviour {
 
+	public GameObject email;
+	public GameObject password;
+
 	// Use this for initialization
 	void Start () {
 	
+	}
+
+	public void LoginF(){
+		
 	}
 	
 	// Update is called once per frame
