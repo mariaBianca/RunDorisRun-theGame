e60c666d00427dbecd94c3a737f5a8137a37e090
Filip_Isakovski <filip.saver15@gmail.com>
2016-12-13 02:31:55 +0100

    updated the chat scene and the chat game scripts

--- Unity/ConnectionScripts/Chat/ChatInput.cs
+++ Unity/ConnectionScripts/Chat/ChatInput.cs
@@ -10,6 +10,10 @@ using uPLibrary.Networking.M2Mqtt.Messages;
 using uPLibrary.Networking.M2Mqtt.Utility;
 using uPLibrary.Networking.M2Mqtt.Exceptions;
 using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 public class ChatInput : MonoBehaviour {
 
@@ -18,21 +22,38 @@ public class ChatInput : MonoBehaviour {
     private string currentMessage = string.Empty;
     private string user = string.Empty;
     private string messageToSend = string.Empty;
+    private ChatMessageObject ChatMessage;
 
     private MqttClient mqttClient;
-    private string MqttSubscribedMessage;
+    private string MqttMessage = string.Empty;
+    private string MqttOldMessage = string.Empty;
 
     bool enterPressed = false;
+
+    [SerializeField]
+    private RectTransform content;
+    [SerializeField]
+    private Text chatLine;
+
+    public ScrollRect chatScrollRect;
+    public Scrollbar chatScrollBar;
+    //private Text newLine;
 
     // Use this for initialization
     void Start () {
         //create an mqtt client instance
         mqttClient = new MqttClient(IPAddress.Parse("129.16.155.34"), 1883, false, null);
+        //mqttClient = new MqttClient(IPAddress.Parse("127.0.0.1"), 1883, false, null);
 
         string clientId = Guid.NewGuid().ToString();
 
         //connect with user, password
         mqttClient.Connect(clientId, "theHub", "theHub");
+        //mqttClient.Connect(clientId);
+
+
+        mqttClient.Subscribe(new string[] { "RunDorisRun/chatroom/potatochat" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+        //mqttClient.Subscribe(new string[] { "test" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
     }
 
     public void SendTextOnClick()
@@ -50,10 +71,12 @@ public class ChatInput : MonoBehaviour {
 
             //temp
             user = "TheDude";
+            int messageId = Random.Range(1000000, 9999999);
 
             //convert message to JSON format
             messageToSend = "{\"text\":\"" + currentMessage + "\"," +
-                            "\"user\":\"" + user + "\"}";
+                            "\"user\":\"" + user + "\"," +
+                            "\"messageId\":\"" + messageId + "\"}";
 
             //sent through mqtt here
             if (mqttClient.IsConnected)
@@ -66,14 +89,44 @@ public class ChatInput : MonoBehaviour {
         }
     }
 
+    void showLatestMessage()
+    {
+        ChatMessage = JsonConvert.DeserializeObject<ChatMessageObject>(MqttMessage);
+        var newLine = ((GameObject)Instantiate(chatLine.gameObject)).GetComponent<Text>();
+        string receivedMessage = ChatMessage.user + ": " + ChatMessage.text;
+        newLine.text = receivedMessage;
+        Debug.Log(receivedMessage);
+        newLine.gameObject.SetActive(true);
+        newLine.rectTransform.SetParent(content);
+
+        Canvas.ForceUpdateCanvases();
+    }
+
+    void MqttMsgGetString(object sender, MqttMsgPublishEventArgs e)
+    {
+        MqttMessage = System.Text.Encoding.UTF8.GetString(e.Message);
+    }
 
     // Update is called once per frame
     void Update () {
-
+        mqttClient.MqttMsgPublishReceived += MqttMsgGetString;
         //focus on InputField
         if (!ChatInputFieldText.isFocused)
         {
             ChatInputFieldText.Select();
+        }
+        Canvas.ForceUpdateCanvases();
+
+        Debug.Log(MqttMessage);
+
+        if (!MqttOldMessage.Equals(MqttMessage) && !MqttMessage.Equals(string.Empty))
+        {
+            showLatestMessage();
+
+            chatScrollRect.verticalNormalizedPosition = 0.0f;
+            chatScrollRect.verticalScrollbar.value = 0f;
+
+            MqttOldMessage = MqttMessage;
         }
 
         //send message on Enter key
@@ -82,5 +135,6 @@ public class ChatInput : MonoBehaviour {
             //Debug.Log("Enter Pressed!");
             SendTextOnClick();
         }
+        Canvas.ForceUpdateCanvases();
     }
 }
---
+++ Unity/ConnectionScripts/Chat/ChatMessageObject.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChatMessageObject : MonoBehaviour {
+
+    public string text { get; set; }
+    public string user { get; set; }
+    public string messageId { get; set; }
+
+    public ChatMessageObject() { }
+
+    public ChatMessageObject(string Text, string User, string MessageId)
+    {
+        text = Text;
+        user = User;
+        messageId = MessageId;
+    }
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
