6226878384b2b7994300d07b00a5f9a16a2db92c
Maria-Bianca Cindroi <bianca.cindroi@gmail.com>
2017-01-14 20:03:17 +0100

    Create ChangePasswordScript.js

---
+++ Node-Red flows/ChangePasswordScript.js
@@ -0,0 +1,29 @@
+/**
+* This script is used in order to change the already existing password in the database on the remote server.
+* @author TheHub
+* DIT029 H16 Project: Software Architecture for Distributed Systems
+* University of Gothenburg, Sweden 2016
+* This file is part of "Run Doris Run!" game.
+* "Run Doris Run!" game is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* Run Doris Runis distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License
+* along with "Run Doris Run!" game.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+msg.query =
+    JSON.parse(msg.payload.substring(0, 34)+ '}');
+msg.payload = JSON.parse('{' + msg.payload.substring(35));
+return msg;
+
+/*
+msg.query =
+    msg.payload.substring(1, 44);
+msg.payload = JSON.parse('{' + msg.payload.substring(45));
+return msg;
+*/
